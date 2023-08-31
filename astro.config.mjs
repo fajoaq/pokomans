ab5399ca2f5225d653832f5f38cef61d866c7b3c
Francis J. <fajoaq@gmail.com>
2023-08-31 19:39:26 -0400

    migrate to Astro 3

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,10 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import mdx from "@astrojs/mdx";
 import solidJs from "@astrojs/solid-js";
-import image from "@astrojs/image";
 
 import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    tailwind(),
-    mdx(),
-    solidJs(),
-    image({
-      serviceEntryPoint: "@astrojs/image/sharp",
-    }),
-    compress(),
-  ],
+  integrations: [tailwind(), solidJs(), compress()],
 });

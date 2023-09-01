42751b756e5a9aecf2193a1d021b890eb88863de
Francis J. <fajoaq@gmail.com>
2023-08-31 21:25:14 -0400

    added vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import solidJs from "@astrojs/solid-js";
-
+import vercelStatic from "@astrojs/vercel/static";
 import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), solidJs(), compress()],
+  integrations: [tailwind(), solidJs(), compress(), vercelStatic()],
+  output: "static",
+  adapter: vercelStatic(),
 });

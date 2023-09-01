8648a8fe8538b73540a607831b6586ed4e5504eb
Francis J. <fajoaq@gmail.com>
2023-08-31 22:09:05 -0400

    changed Astro Compress settings

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,17 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import solidJs from "@astrojs/solid-js";
 import vercel from "@astrojs/vercel/serverless";
-import compress from "astro-compress";
+import Compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), solidJs(), compress()],
+  integrations: [
+    tailwind(),
+    solidJs(),
+    Compress({
+      SVG: false,
+    }),
+  ],
   output: "server",
   adapter: vercel(),
   functionPerRoute: false,

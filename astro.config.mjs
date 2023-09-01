0fda9f833db75f159bf9012c7a1515da8b9dd195
Francis J. <fajoaq@gmail.com>
2023-08-31 21:37:12 -0400

    changed vercel adapter settings

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import solidJs from "@astrojs/solid-js";
-import vercelStatic from "@astrojs/vercel/static";
+import vercel from "@astrojs/vercel/serverless";
 import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), solidJs(), compress(), vercelStatic()],
-  output: "static",
-  adapter: vercelStatic(),
+  integrations: [tailwind(), solidJs(), compress()],
+  output: "server",
+  adapter: vercel(),
+  functionPerRoute: false,
 });
